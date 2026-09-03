c91409c9dc6eef80c97ba9a971692a4e06ccbbbd
Ozywek <138172500+Ozywek@users.noreply.github.com>
2026-09-03 17:12:00 +0200

    Delete 17.py

--- 17.py
+++
@@ -1,9 +0,0 @@
-def best_player(scores):
-    best_score = next(iter(scores.values()))
-    best_player = next(iter(scores.keys()))
-    for i in scores:
-        if scores[i] > best_score:
-            best_score = scores[i]
-            best_player = i
-    return best_player
-
