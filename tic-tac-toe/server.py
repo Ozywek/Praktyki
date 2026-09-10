5c74c29b440655973c2fbcbd3b2b8ca3d90fbb45
Ozywek <dlugaibezsensownanazwa@gmail.com>
2026-09-10 15:13:36 +0200

    Fixes

--- tic-tac-toe/server.py
+++ tic-tac-toe/server.py
@@ -1,4 +1,3 @@
-import app
 from fastapi import FastAPI
 from typing import Annotated
 from pydantic import BaseModel, Field
@@ -24,7 +23,7 @@ class Game:
         "winner": None,
         "winning_line": None,
         "draw": False,
-        "finished": False
+        "finished": False,
     }
 
     def reset(self):
@@ -36,6 +35,7 @@ class Game:
         "draw": False,
         "finished": False
     }
+        return self.dict
     def check_winner(self):
         winning = [
             (0, 1, 2),
@@ -61,22 +61,23 @@ class Game:
     def one_move(self, ind):
         self.dict["board"][ind] = self.dict["current"]
 
-        if self.check_draw() == False:
-            if (a := self.check_winner()) == False:
-            # if self.check_winner() == False:
+        if (a := self.check_winner()) == False:
+            if self.check_draw() == False:
                 if self.dict["current"] == "X":
                     self.dict["current"] = "O"
                 else:
                     self.dict["current"] = "X"
             else:
-                self.dict["winning_line"] = a
-                self.dict["winner"] = self.dict["current"]
+                self.dict["draw"] = True
                 self.dict["finished"] = True
+                self.dict["current"] = None
         else:
-            self.dict["draw"] = True
+            self.dict["winning_line"] = a
+            self.dict["winner"] = self.dict["current"]
             self.dict["finished"] = True
-
+            self.dict["current"] = None
         return self.dict
+
     def is_finished(self):
         if self.dict["finished"]:
             return  True
@@ -84,21 +85,24 @@ class Game:
             return False
 
     def is_taken(self, ind):
-        if game.dict["board"][ind] != None:
+        if self.dict["board"][ind] != None:
             return True
         else: return False
+
+
+    def get_dict(self):
+            return self.dict
 
 
 game = Game()
 
 @app.get("/game")
 def root():
-    return game.dict
+    return game.get_dict()
 
 @app.post("/game")
 def reset():
-    game.reset()
-    return game.dict
+    return game.reset()
 
 @app.post("/game/move")
 def make_move(move: Move):
